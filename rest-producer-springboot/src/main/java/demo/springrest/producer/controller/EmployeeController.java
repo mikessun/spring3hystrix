66e53df69b8839dbbb9b7fbf0bd404beb1b23f43
Michael Sun <mikessun2010@gmail.com>
2017-02-28 19:29:46 -0800

    adding crud method

--- rest-producer-springboot/src/main/java/demo/springrest/producer/controller/EmployeeController.java
+++ rest-producer-springboot/src/main/java/demo/springrest/producer/controller/EmployeeController.java
@@ -3,24 +3,46 @@ package demo.springrest.producer.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import demo.springrest.producer.model.Employee;
 
+import javax.ws.rs.PathParam;
+
 @RestController
+
 public class EmployeeController {
-	static List<Employee> employeesList = new ArrayList<Employee>();
-	static{
-		employeesList.add(new Employee(1,"Michael","Novo","howtodoinjava@gmail.com"));
-		employeesList.add(new Employee(2,"David","Johnson","howtodoinjava@gmail.com"));
-	}
-	
-	@RequestMapping("/")
-    public List<Employee> getEmployees() 
-    {
-		
-		return employeesList;
+    static List<Employee> employeesList = new ArrayList<Employee>();
+
+    static {
+        employeesList.add(new Employee(1, "Michael", "Novo", "howtodoinjava@gmail.com"));
+        employeesList.add(new Employee(2, "David", "Johnson", "howtodoinjava@gmail.com"));
+    }
+
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
+    public List<Employee> getEmployees() {
+        return employeesList;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}")
+    public ResponseEntity deleteEmployees(@PathVariable(value = "id") Integer id) {
+        boolean removed = employeesList.removeIf(p -> p.getId().intValue() == id.intValue());
+        System.out.println("removed=" + removed);
+        if (!removed) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/add")
+    public ResponseEntity addEmployees(@RequestBody Employee input) {
+        employeesList.add(input);
+        return ResponseEntity.ok().build();
     }
 
 }
